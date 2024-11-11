597c9ee89f771bca5452bf49dade553f9f9e2804
vishwas2002 <vishuashoks@gmail.com>
2024-11-11 17:47:15 +0530

    Initial commit

---
+++ src/components/Tagline.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import brackets from '../assets/svg/Brackets'
+const Tagline = ({ className, children }) => {
+    return (
+        <div className={`tagline flex item-center ${className || " "}`}>{brackets("left")}
+            <div className="mx-3 text-n-3">{children}</div>
+            {brackets("right")}
+        </div>
+    )
+}
+
+export default Tagline
